54b57f8f9bf74477e3406d9225ed7986029208f7
TakamasaYoshizawa <54523495+TakamasaYoshizawa@users.noreply.github.com>
2019-11-20 13:15:44 +0900

    Delete test03.js

--- techboost/test03.js
+++
@@ -1,8 +0,0 @@
-(function($, window) {
-    $(function() {
-      // ここに処理を書きます
-      $('html').on('click', function() {
-        $(this).css({'color': 'red'});
-      });
-    });
-  })(jQuery, window);
